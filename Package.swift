bc2a0140f1572ce7e7ef5d48822b54b563de7bbd
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-09-30 18:39:20 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -6,5 +6,22 @@ let package = Package(
     name: "CFFmpeg",
     products: [.library(name: "CFFmpeg", targets: ["CFFmpeg"])],
     dependencies: [],
-    targets: [.systemLibrary(name: "CFFmpeg")]
+    targets: [
+        .target(name: "CFFmpeg", dependencies: [
+            "avcodec",
+            "avdevice",
+            "avfilter",
+            "avformat",
+            "avutil",
+            "swresample",
+            "swscale",
+          ]),
+        .binaryTarget(name: "avcodec", path: "xcframeworks/avcodec.xcframework"),
+        .binaryTarget(name: "avdevice", path: "xcframeworks/avdevice.xcframework"),
+        .binaryTarget(name: "avfilter", path: "xcframeworks/avfilter.xcframework"),
+        .binaryTarget(name: "avformat", path: "xcframeworks/avformat.xcframework"),
+        .binaryTarget(name: "avutil", path: "xcframeworks/avutil.xcframework"),
+        .binaryTarget(name: "swresample", path: "xcframeworks/swresample.xcframework"),
+        .binaryTarget(name: "swscale", path: "xcframeworks/swscale.xcframework")
+    ]
 )
