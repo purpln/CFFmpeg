3cf1c309aa158fedd13d47ef0063621a4f811ba0
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2021-10-29 20:00:30 +0300

    no message

--- Sources/CFFmpeg/CFFmpeg.h
+++ Sources/CFFmpeg/CFFmpeg.h
@@ -1,12 +1,12 @@
-//
-//  CFFmpeg.h
-//  CFFmpeg
-//
-//  Created by Sergey Romanenko on 29.10.2021.
-//
-
 #ifndef CFFmpeg_h
 #define CFFmpeg_h
 
+#include "headers/avutil.h"
+#include "headers/avformat.h"
+#include "headers/avcodec.h"
+#include "headers/avfilter.h"
+#include "headers/avdevice.h"
+#include "headers/swscale.h"
+#include "headers/swresample.h"
 
-#endif /* CFFmpeg_h */
+#endif
